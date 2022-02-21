283c9c94be89b8f6873b1187b0b2a6feceb807b7
Parita2910 <PA16@myscc.ca>
2022-02-20 21:28:59 -0500

    Assignment3

--- A_Parita_MyFavouritessportperson/src/app/app.module.ts
+++ A_Parita_MyFavouritessportperson/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
+import { FiltersPipe } from './filters.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
-    ContentListComponent
+    ContentListComponent,
+    FiltersPipe
   ],
   imports: [
     BrowserModule
