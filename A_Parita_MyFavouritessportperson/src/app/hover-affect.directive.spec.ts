97aef6944a2151899a72185da51f1b5956e0a90f
Parita2910 <PA16@myscc.ca>
2022-02-20 23:47:55 -0500

    Assignment4

---
+++ A_Parita_MyFavouritessportperson/src/app/hover-affect.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HoverDirective } from './hover-affect.directive';
+
+describe('HoverDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HoverAffectDirective();
+    expect(directive).toBeTruthy();
+  });
+});
