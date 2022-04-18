9edb4699bcddecc99921d6b1d6a72dbd4a294a67
Parita2910 <PA16@myscc.ca>
2022-04-17 23:00:45 -0400

    fdz

--- A_Parita_MyFavouritessportperson/src/app/app.module.ts
+++ A_Parita_MyFavouritessportperson/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { FiltersPipe } from './filters.pipe';
 import { HoverDirective } from './hover-affect.directive';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,13 @@ import { HoverDirective } from './hover-affect.directive';
     HoverDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
